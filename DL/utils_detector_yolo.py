38485d4673e01d41d6d12928e6024f21727419c8
dryabokon <d.ryabokon@gmail.com>
2025-10-15 16:13:34 +0300

    GPU

--- DL/utils_detector_yolo.py
+++ DL/utils_detector_yolo.py
@@ -23,7 +23,7 @@ class Detector_yolo:
         self.folder_out = folder_out
 
         self.model_detect = YOLO(model_name,task="detect")
-        self.imgsz = self.config.imgsz
+        self.imgsz = config.imgsz
         self.colors80 = tools_draw_numpy.get_colors(80, colormap='nipy_spectral', shuffle=True)
         self.dct_class_names = None
         return
